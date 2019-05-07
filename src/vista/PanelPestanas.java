a64f613b60d94273d53887ec6e77b2bda700a14f
Gabriel Blanco La Rotta <geblarot96@gmail.com>
2019-05-06 21:11:50 -0500

    askjdgb
    Dunno

--- src/vista/PanelPestanas.java
+++ src/vista/PanelPestanas.java
@@ -1,4 +1,26 @@
 package vista;
 
+import javax.swing.*;
+import java.awt.*;
 
+public class PanelPestanas extends JPanel
+{
+	private static final long serialVersionUID = 1L;
+	private JTabbedPane pestanas;
+	private PanelSuperAstro psa;
+	private PanelBaloto pb;
+	private PanelOhPolla poh;
+	
+	public PanelPestanas()
+	{
+		setLayout(new GridLayout());
+		pestanas = new JTabbedPane();
+		psa = new PanelSuperAstro();
+		pb = new PanelBaloto();
+		poh = new PanelOhPolla();
+		add(pestanas);
+		pestanas.addTab("SuperAstro", psa);
+		pestanas.addTab("Baloto", pb);
+		pestanas.addTab("Marcardor Oh!Polla", poh);
+	}
 }
