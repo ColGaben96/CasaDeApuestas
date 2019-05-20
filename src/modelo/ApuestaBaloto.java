293bdfb109a4905bd95f3cab7a036ef0b6725455
jsantosnio1 <49841818+jsantosnio1@users.noreply.github.com>
2019-05-19 21:05:42 -0500

    Update ApuestaBaloto.java

--- src/modelo/ApuestaBaloto.java
+++ src/modelo/ApuestaBaloto.java
@@ -14,10 +14,13 @@ public class ApuestaBaloto {
 			
 	//	}
 		
-		int a = 0, b = 0; 
+		int a = 0, b = 0, c = 0 , d = 0 , e=0; 
 		
 		a = (int)((int)Math.random()*Math.rint(45));
 		b = (int)((int)Math.random()*Math.rint(45));
+		c = (int)((int)Math.random()*Math.rint(45));
+		d = (int)((int)Math.random()*Math.rint(45));
+		d = (int)((int)Math.random()*Math.rint(45));
 		
 	//	int x = 0 , y = 0;
 		
