0ebe567c359f043ff0b164a2cd7c61b4b818f434
jsantosnio1 <49841818+jsantosnio1@users.noreply.github.com>
2019-05-19 20:49:21 -0500

    m
    Baloto

--- src/modelo/ApuestaBaloto.java
+++ src/modelo/ApuestaBaloto.java
@@ -1,5 +1,34 @@
 package modelo;
 
+import java.util.LinkedList;
+
 public class ApuestaBaloto {
+	
+	public void Automatico(){
+		
+	//	LinkedList <Integer> lista = new LinkedList();
+		
+	//	for (int i = 0; i < 45; i++) {
+			
+	//		lista.add(i);
+			
+	//	}
+		
+		int a = 0, b = 0; 
+		
+		a = (int)((int)Math.random()*Math.rint(45));
+		b = (int)((int)Math.random()*Math.rint(45));
+		
+	//	int x = 0 , y = 0;
+		
+	//	x =lista.get(a);
+	//	lista.remove(a);
+		
+	//	x =lista.get(b);
+	//	lista.remove(b);
+		
+		
+		
+	}
 
 }
