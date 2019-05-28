342f4a63f81e7d703577ea7e826ced0f45d84c6d
jsantosnio1 <49841818+jsantosnio1@users.noreply.github.com>
2019-05-28 10:22:25 -0500

    Apuesta

--- src/modelo/ApuestaBaloto.java
+++ src/modelo/ApuestaBaloto.java
@@ -7,15 +7,43 @@ public class ApuestaBaloto extends Apuestas
 	public String BalotoAutomatico(){
 		
 		String num=" ";
+		int a = 0, b = 0, c = 0 , d = 0 , e=0 , f=0; 
+	
+		while(a<40) {
+			
+			a = (int)(Math.rint(45)*Math.random());
+			
+		}
+		while(b<35) {
+			
+			b = (int)(Math.rint(40)*Math.random());
+			
+		}
+		while(c<30) {
+			
+			c = (int)(Math.rint(35)*Math.random());
+		}
 		
-		a = (int)(Math.rint(44)*Math.random());
-		b = (int)(Math.rint(44)*Math.random());
-		c = (int)(Math.rint(44)*Math.random());
-		d = (int)(Math.rint(44)*Math.random());
-		e = (int)(Math.rint(44)*Math.random());
-		f = (int)(Math.rint(44)*Math.random());
+		while(d<25) {
+			
+			d = (int)(Math.rint(30)*Math.random());
+		}
 		
-		num= (Integer.toString(a) + Integer.toString(b) + Integer.toString(c) + Integer.toString(d) + Integer.toString(e) +Integer.toString(f));
+		while(e<20) {
+			
+			e = (int)(Math.rint(25)*Math.random());
+			
+		}
+		
+		while(f<15) {
+			
+			f = (int)(Math.rint(20)*Math.random());
+			
+		}
+		
+
+		
+		num= (Integer.toString(a) +"-"+  Integer.toString(b) +"-"+ Integer.toString(c) +"-"+ Integer.toString(d) +"-"+ Integer.toString(e) +"-"+Integer.toString(f));
 	
 		return num;
 		
